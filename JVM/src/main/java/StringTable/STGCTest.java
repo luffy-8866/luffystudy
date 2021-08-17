d11a3ffdf4e233f6aa625a0556554231be1556f8
luffy <zhanglf@i2finance.net>
2021-08-17 10:57:15 +0800

    1.

---
+++ JVM/src/main/java/StringTable/STGCTest.java
@@ -0,0 +1,13 @@
+package main.java.StringTable;
+
+/**
+ * @author luffy
+ * @version 1.0
+ * @desc
+ * @date 2021/7/22 10:23
+ */
+public class STGCTest {
+    public static void main(String[] args) {
+
+    }
+}
--- juc/src/main/java/com/luffy/juc/test.java
+++ juc/src/main/java/com/luffy/juc/test.java
@@ -10,17 +10,30 @@ import java.util.concurrent.TimeUnit;
  */
 public class test {
 
-    public static void main(String[] args) {
-        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
-        Phone a = new Phone();
-        new Thread(()->{a.msg();},"B").start();
-        try {
-            TimeUnit.SECONDS.sleep(1);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    static int m1(int num){
+        int x=0;
+        for (int i=0;i<num;i++){
+            x++;
         }
-        new Thread(()->{a.send();},"A").start();
+        return x;
+    }
 
+    public static void main(String[] args) {
+//        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
+//        Phone a = new Phone();
+//        new Thread(()->{a.msg();},"B").start();
+//        try {
+//            TimeUnit.SECONDS.sleep(1);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        new Thread(()->{a.send();},"A").start();
+        new Thread(()->{
+            System.out.println(m1(1000));
+        }).start();
+        new Thread(()->{
+            System.out.println("A"+m1(2000));
+        }).start();
 
     }
 
